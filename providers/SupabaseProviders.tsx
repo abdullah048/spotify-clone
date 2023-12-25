b852cad8d6972d3ae7edec4708ccc3b5c74a834f
abdullah048 <abdullahshahbaz048@gmail.com>
2023-12-25 22:06:55 +0500

    feat: working on supabase providers

---
+++ providers/SupabaseProviders.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { FC, ReactNode } from 'react';
+
+interface SupabaseProvidersProps {
+  children: ReactNode;
+}
+
+const SupabaseProviders: FC<SupabaseProvidersProps> = ({}) => {
+  return <div>SupabaseProviders</div>;
+};
+
+export default SupabaseProviders;
